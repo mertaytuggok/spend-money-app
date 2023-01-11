453837eefb026f32c0306c3719c151ef8a7a90de
mertaytuggok <mertaytuggok@gmail.com>
2023-01-11 21:44:11 +0300

    UI changed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Header } from "./Components/Header/Header";
+import { Product } from "./Components/Product/Product";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
