3e58cef6638fb31660b0a9bf359558ed6021149c
mertaytuggok <mertaytuggok@gmail.com>
2023-01-17 16:26:57 +0300

    project fixed

--- src/Redux/ReduxSlice/productSlice.js
+++ src/Redux/ReduxSlice/productSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import Product from "../../Product/Product.json";
 
-const data = Product.products;
+const data = Product;
+
 export const productsSlice = createSlice({
   name: "products",
   initialState: {
@@ -9,7 +10,23 @@ export const productsSlice = createSlice({
     budget: 100000000000,
     initialMoney: 100000000000,
   },
-  reducers: {},
+  reducers: {
+    updateCount: (state, action) => {
+      const { count, id } = action.payload;
+
+      const product = state.items.find((product) => product.id === id);
+      product.count = count;
+
+      let price = 0;
+
+      state.items.forEach((product) => {
+        price += Number(product.count) * Number(product.productPrice);
+      });
+
+      state.budget = state.initialMoney - Number(price);
+    },
+  },
 });
 
+export const { updateCount } = productsSlice.actions;
 export default productsSlice.reducer;
